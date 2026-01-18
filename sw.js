397becd8e1940e1030af447f12cd76ec92d3e718
Mesaque <imesalink@gmail.com>
2026-01-18 14:07:00 -0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'frota-itaquera-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-pneu.png',
+  './icon-chave.png',
+  './SescLogo.png',
+  './favicon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
